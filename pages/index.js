697b8bc7ce9066e9c6a107af4b444f59a84687ff
Niall Eccles <nialle@rothco.ie>
2022-06-17 23:12:18 +0100

    Created palette component and added some dummy palettes.

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,27 @@
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
+import Palette from "../components/palette";
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      <div class="hero min-h-screen">
-        <div class="hero-content text-center">
-          <div class="max-w-md">
-            <h1 class="text-5xl font-bold">kleur</h1>
-          </div>
+      <div className="navbar bg-base-100">
+        <div className="flex-1">
+          <a className="btn btn-ghost normal-case text-xl font-bold kleur-title">
+            kleur
+          </a>
         </div>
+      </div>
+      <div className={styles.paletteGrid}>
+        <Palette colours={['#F47C7C','#EF9F9F', '#FAD4D4', '#FFF2F2']}/>
+        <Palette colours={['#293462','#F24C4C', '#EC9B3B', '#F7D716']}/>
+        <Palette colours={['#293462','#F24C4C', '#F7D716']}/>
+        <Palette colours={['#F7D716','#F24C4C', ]}/>
+        <Palette colours={['#293462','#F24C4C', '#F7D716']}/>
+        <Palette colours={['#293462','#F24C4C', '#EC9B3B', '#F7D716']}/>
+        <Palette colours={['#F7D716','#F24C4C', ]}/>
+        <Palette colours={['#293462','#F24C4C', '#EC9B3B', '#F7D716']}/>
       </div>
     </div>
   );
