c7713adab96756ab7d5c5d8c3b56852d7bba73bb
Niall Eccles <nialle@rothco.ie>
2022-06-18 21:11:30 +0100

    Decoupled data from views.

---
+++ data/palettes.js
@@ -0,0 +1,35 @@
+export const palettes = [
+    {
+      colours: ["#F47C7C", "#EF9F9F", "#FAD4D4", "#FFF2F2"],
+      tags: ["warm"],
+    },
+    {
+      colours: ["#293462", "#F24C4C", "#EC9B3B", "#F7D716"],
+      tags: ["warm"],
+    },
+    {
+      colours: ["#293462", "#F24C4C", "#F7D716"],
+      tags: ["warm"],
+    },
+    {
+      colours: ["#F7D716", "#F24C4C"],
+      tags: ["warm"],
+    },
+    {
+      colours: ["#293462", "#F24C4C", "#F7D716"],
+      tags: ["warm"],
+    },
+    {
+      colours: ["#293462", "#F24C4C", "#EC9B3B", "#F7D716"],
+      tags: ["warm"],
+    },
+    {
+      colours: ["#F7D716", "#F24C4C"],
+      tags: ["warm"],
+    },
+    {
+      colours: ["#293462", "#F24C4C", "#EC9B3B", "#F7D716"],
+      tags: ["warm"],
+    },
+  ];
+  
