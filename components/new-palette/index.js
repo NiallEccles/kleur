b9d5a52af85cf5e06fa0690c3937505f63edc93f
Niall Eccles <nialle@rothco.ie>
2022-06-21 22:12:17 +0100

    Prep for colour picker in new palette.

--- components/new-palette/index.js
+++ components/new-palette/index.js
@@ -1,12 +1,15 @@
 import { useState } from "react";
 import styles from "./new-palette.module.css";
+import { HexColorPicker } from "react-colorful";
 
 // const controls = [{ colour: "#E23E57" }, { colour: "#88304E" }];
 
 export default function NewPalette() {
   const [controls, setControl] = useState([
-    { colour: "#E23E57" },
-    { colour: "#88304E" },
+    { colour: "#" },
+    { colour: "#" },
+    { colour: "#" },
+    { colour: "#" },
   ]);
   return (
     <div className={styles.paletteContainer}>
@@ -27,55 +30,63 @@ export default function NewPalette() {
                 });
               }}
             />
-            <button className={styles.copyIcon}>{copyIcon()}</button>
+            <button className={styles.paletteIcon}>{paletteIcon()}</button>
+            {/* {colourPicker(showPicker, setShowPicker, controls, setControl, index)} */}
           </div>
         ))}
       </div>
-      <button
+      {/* <button
         className={`btn btn-ghost mt-4 w-full ${styles.addIcon}`}
         onClick={() => {
           newControl(controls, setControl);
         }}
       >
         {controls.length < 4 ? "Add Sliver" : "Max Slivers Reached"} {addIcon()}
-      </button>
+      </button> */}
     </div>
   );
 }
 
-function copyIcon() {
+function colourPicker(showPicker, setShowPicker, controls, setControl, index) {
+  return (
+    <div
+      className={`${styles.colourPicker} ${
+        controls[index].showPicker ? styles.showPicker : ""
+      }`}
+    >
+      <HexColorPicker
+        color={"#ffffff"}
+        onChange={(e) => {
+          updateControl(controls, setControl, index, {
+            colour: e,
+          });
+        }}
+      />
+    </div>
+  );
+}
+
+function paletteIcon() {
   return (
     <svg
       id="Layer_1"
       xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 37.65 43.6"
+      viewBox="0 0 37.8 37.8"
     >
-      <path d="M12,36.2c-1.27,0-2.34-.46-3.23-1.38s-1.33-1.97-1.33-3.17V4.55c0-1.2,.44-2.26,1.33-3.17,.88-.92,1.96-1.38,3.23-1.38h21.05c1.27,0,2.35,.46,3.25,1.38,.9,.92,1.35,1.97,1.35,3.17V31.65c0,1.2-.45,2.26-1.35,3.17s-1.98,1.38-3.25,1.38H12Zm0-4.55h21.05V4.55H12V31.65h0Zm-7.4,11.95c-1.27,0-2.35-.46-3.25-1.38s-1.35-1.98-1.35-3.17V9.6H4.6v29.45H27.55v4.55H4.6ZM12,4.55h0V31.65h0V4.55h0Z" />
+      <path d="M0,37.8v-9.5L18.95,9.35l-3.45-3.5,2.8-2.8,4.45,4.5L29.55,.7c.5-.47,1.02-.7,1.55-.7s1.03,.25,1.5,.75l4.45,4.45c.5,.47,.75,.97,.75,1.5,0,.53-.23,1.05-.7,1.55l-6.8,6.75,4.45,4.5-2.8,2.8-3.45-3.5L9.5,37.8H0Zm3.85-3.85h4.25L25.85,16.2l-4.25-4.25L3.85,29.7v4.25Z" />
     </svg>
   );
-}
-
-function addIcon() {
-  return (
-    <svg
-      id="Layer_1"
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 29.55 29.5"
-    >
-      <path d="M12.5,29.5v-12.45H0v-4.55H12.5V0h4.55V12.5h12.5v4.55h-12.5v12.45h-4.55Z" />
-    </svg>
-  );
-}
-
-function newControl(controls, setControl) {
-  if (controls.length < 4) {
-    let newArr = [...controls, { colour: "#" }];
-    setControl(newArr);
-  }
 }
 
 function updateControl(controls, setControls, index, newValue) {
   let newArr = [...controls];
   newArr[index] = newValue;
   setControls(newArr);
+}
+
+function togglePicker(controls, setControls, index) {
+  let newArr = [...controls];
+  newArr[index].showPicker = !newArr[index].showPicker;
+  setControls(newArr);
+  console.log(controls);
 }
