c1278bdbcf793f24e03893fa49d34e504f789235
Niall Eccles <nialle@rothco.ie>
2022-09-08 21:19:56 +0100

    Moved local palettes and local gradients into new fragments.

---
+++ fragments/local-gradients.js
@@ -0,0 +1,28 @@
+import styles from "../styles/Home.module.css";
+import { useEffect, useState } from "react";
+import Gradient from "../components/gradient";
+
+export default function LocalGradients() {
+  const [localGradients, setLocalGradients] = useState([]);
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      setLocalGradients(JSON.parse(window.localStorage.getItem("gradients")));
+    }
+  }, []);
+
+  return localGradients != null ? (
+    <>
+      <h2 className="text-2xl font-bold ml-5">My Gradients</h2>
+      <div className={styles.paletteGrid}>
+        {localGradients.length > 0
+          ? localGradients.map((gradient, index) => {
+              return <Gradient key={index} colours={gradient} />;
+            })
+          : ""}
+      </div>
+    </>
+  ) : (
+    ""
+  );
+}
