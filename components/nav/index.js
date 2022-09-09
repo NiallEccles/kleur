4911fb865699f5f02a7ba8611fa3c2268ee8daf9
Niall Eccles <nialle@rothco.ie>
2022-09-09 22:52:29 +0100

    Fixed react prop errors. Made some mobile tweaks.

--- components/nav/index.js
+++ components/nav/index.js
@@ -54,12 +54,12 @@ export default function Nav() {
                   xmlns="http://www.w3.org/2000/svg"
                   viewBox="0 0 24 24"
                   fill="currentColor"
-                  class="w-6 h-6 mr-2"
+                  className="w-6 h-6 mr-2"
                 >
                   <path
                     fillRule="evenodd"
                     d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25zM12.75 9a.75.75 0 00-1.5 0v2.25H9a.75.75 0 000 1.5h2.25V15a.75.75 0 001.5 0v-2.25H15a.75.75 0 000-1.5h-2.25V9z"
-                    clip-rule="evenodd"
+                    clipRule="evenodd"
                   />
                 </svg>
                 Create
@@ -71,7 +71,7 @@ export default function Nav() {
                 <li>
                   {" "}
                   <Link href="/new/palette">
-                    <a className="flex font-semibold items-center">
+                    <a className="flex font-semibold items-center active:bg-black">
                       <svg
                         xmlns="http://www.w3.org/2000/svg"
                         viewBox="0 0 24 24"
@@ -91,7 +91,7 @@ export default function Nav() {
                 </li>
                 <li>
                   <Link href="/new/gradient">
-                    <a className="flex font-semibold items-center">
+                    <a className="flex font-semibold items-center active:bg-black">
                       <svg
                         xmlns="http://www.w3.org/2000/svg"
                         viewBox="0 0 24 24"
