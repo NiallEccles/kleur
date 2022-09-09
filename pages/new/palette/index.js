5732134d50c9f283a6afe41e5bf529f569186d93
Niall Eccles <nialle@rothco.ie>
2022-09-09 22:37:32 +0100

    Updared new palette and new gradient pages to use nav component.

--- pages/new/palette/index.js
+++ pages/new/palette/index.js
@@ -1,20 +1,12 @@
 import Link from "next/link";
 import styles from "../../../styles/Home.module.css";
 import NewPalette from "../../../components/new-palette";
-import { PAGES } from "/public/shared.constants";
+import Nav from "../../../components/nav";
 
 export default function PaletteNew() {
   return (
     <div className={styles.container}>
-      <div className="navbar bg-base-100">
-        <div className="flex-1">
-          <Link href={PAGES.HOME}>
-            <a className="btn btn-ghost normal-case text-xl font-bold kleur-title">
-              kleur
-            </a>
-          </Link>
-        </div>
-      </div>
+      <Nav />
       <NewPalette />
     </div>
   );
