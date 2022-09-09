76f3614480f9cd16a19ff9863b3cbcb0f7ab09e0
Niall Eccles <nialle@rothco.ie>
2022-09-09 19:40:58 +0100

    Added in hero text for palettes and gradients pages.

--- pages/gradients/index.js
+++ pages/gradients/index.js
@@ -4,6 +4,7 @@ import { gradients } from "../../data/gradients";
 import Nav from "../../components/nav";
 import Gradient from "../../components/gradient";
 import { motion } from "framer-motion";
+import { useState } from "react";
 
 const container = {
   hidden: { opacity: 1, scale: 0 },
@@ -26,10 +27,19 @@ const item = {
 };
 
 export default function Index() {
+  const [isVisible, setIsVisible] = useState(false);
+
+  setTimeout(() => {
+    setIsVisible(true);
+  }, 300);
   return (
     <div className={homeStyles.container}>
       <Nav />
-      <h2 className="text-2xl font-bold ml-5">Gradients</h2>
+      <motion.div initial={false} animate={{ opacity: isVisible ? 1 : 0 }}>
+        <div className="my-8 py-40 rounded-3xl">
+          <h1 className="text-9xl font-bold text-center">Gradients</h1>
+        </div>
+      </motion.div>
       <motion.div
         className={gradientStyles.grid}
         variants={container}
