6d7aad0657265ad70f2aef88b81dfcd6359f5979
Niall Eccles <nialle@rothco.ie>
2022-09-09 13:10:39 +0100

    Added framer motion for some nice animations.

--- components/hero/hero.js
+++ components/hero/hero.js
@@ -1,13 +1,23 @@
+import { motion } from "framer-motion"
+import { useState } from "react";
+
 export default function Hero() {
+  const [isVisible, setIsVisible] = useState(false);
+
+  setTimeout(()=>{
+    setIsVisible(true);
+  },300)
   return (
-    <div className="my-8 py-40 rounded-3xl">
-      <h1 className="text-9xl font-bold text-center">kleur</h1>
-      <h2 className="text-xl font-bold text-center">
-        <span className="italic">colour</span> (English)
-      </h2>
-      <h3 className="text-4xl font-bold text-center mt-16">
-        All about colour.
-      </h3>
-    </div>
+    <motion.div initial={false} animate={{ opacity: isVisible ? 1 : 0 }}>
+      <div className="my-8 py-40 rounded-3xl">
+        <h1 className="text-9xl font-bold text-center">kleur</h1>
+        <h2 className="text-xl font-bold text-center">
+          <span className="italic">colour</span> (English)
+        </h2>
+        <h3 className="text-4xl font-bold text-center mt-16">
+          All about colour.
+        </h3>
+      </div>
+    </motion.div>
   );
 }
