0076b4fbbf05fd0c9b365e124d9f9778284bc3bf
EcclesN <Niall.Eccles@hmhco.com>
2023-10-21 11:00:21 +0100

    Add experimental label

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -4,7 +4,22 @@ import '../styles/globals.css';
 // eslint-disable-next-line react/prop-types
 function MyApp({ Component, pageProps }) {
   // eslint-disable-next-line react/jsx-props-no-spreading
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <span style={{
+        position: 'fixed',
+        bottom: '1em',
+        right: '1em',
+        background: '#2b2b2b',
+        color: 'white',
+        padding: '0.25em',
+      }}
+      >
+        Experimental
+      </span>
+    </>
+  );
 }
 
 export default MyApp;
