da5d8a11207d90411b374e6799541b51c2c39b01
EcclesN <Niall.Eccles@hmhco.com>
2025-02-03 17:54:07 +0000

    Rework nav to use navlink component

--- components/nav/index.js
+++ components/nav/index.js
@@ -1,6 +1,25 @@
 import Link from "next/link";
+import NavLink from "../nav-link/nav-link";
+import { PaletteIcon, SwatchBook, CirclePlus } from "lucide-react";
 
 export default function Nav() {
+    const menuItems = [
+        {
+            path: '/palettes',
+            label: 'Palettes',
+            icon: <PaletteIcon />,
+        },
+        {
+            path: '/gradients',
+            label: 'Gradients',
+            icon: <SwatchBook />,
+        },
+        {
+            path: '/create',
+            label: 'Create',
+            icon: <CirclePlus />,
+        },
+    ];
     return (
         <nav className="navbar bg-base-100">
             <div className="flex-1">
@@ -8,117 +27,13 @@ export default function Nav() {
                     kleur
                 </Link>
             </div>
-            <div>
-                <ol className="flex">
-                    <li className="btn btn-ghost normal-case">
-                        <Link href="/palettes" className="flex font-semibold items-center">
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                viewBox="0 0 24 24"
-                                fill="currentColor"
-                                className="w-6 h-6 mr-2"
-                            >
-                                <path
-                                    fillRule="evenodd"
-                                    d="M2.25 4.125c0-1.036.84-1.875 1.875-1.875h5.25c1.036 0 1.875.84 1.875 1.875V17.25a4.5 4.5 0 11-9 0V4.125zm4.5 14.25a1.125 1.125 0 100-2.25 1.125 1.125 0 000 2.25z"
-                                    clipRule="evenodd"
-                                />
-                                <path
-                                    d="M10.719 21.75h9.156c1.036 0 1.875-.84 1.875-1.875v-5.25c0-1.036-.84-1.875-1.875-1.875h-.14l-8.742 8.743c-.09.089-.18.175-.274.257zM12.738 17.625l6.474-6.474a1.875 1.875 0 000-2.651L15.5 4.787a1.875 1.875 0 00-2.651 0l-.1.099V17.25c0 .126-.003.251-.01.375z"/>
-                            </svg>
-                            Palettes
-                        </Link>
-                    </li>
-                    <li className="btn btn-ghost normal-case flex font-semibold items-center">
-                        <Link href="/gradients" className="flex font-semibold items-center">
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                viewBox="0 0 24 24"
-                                fill="currentColor"
-                                className="w-6 h-6 mr-2"
-                            >
-                                <path
-                                    d="M5.566 4.657A4.505 4.505 0 016.75 4.5h10.5c.41 0 .806.055 1.183.157A3 3 0 0015.75 3h-7.5a3 3 0 00-2.684 1.657zM2.25 12a3 3 0 013-3h13.5a3 3 0 013 3v6a3 3 0 01-3 3H5.25a3 3 0 01-3-3v-6zM5.25 7.5c-.41 0-.806.055-1.184.157A3 3 0 016.75 6h10.5a3 3 0 012.683 1.657A4.505 4.505 0 0018.75 7.5H5.25z"/>
-                            </svg>
-                            Gradients
-                        </Link>
-                    </li>
-                    <li className="btn btn-ghost normal-case">
-                        <div className="dropdown dropdown-end">
-                            <label tabIndex={0} className="flex font-semibold items-center">
-                                <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    viewBox="0 0 24 24"
-                                    fill="currentColor"
-                                    className="w-6 h-6 mr-2"
-                                >
-                                    <path
-                                        fillRule="evenodd"
-                                        d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25zM12.75 9a.75.75 0 00-1.5 0v2.25H9a.75.75 0 000 1.5h2.25V15a.75.75 0 001.5 0v-2.25H15a.75.75 0 000-1.5h-2.25V9z"
-                                        clipRule="evenodd"
-                                    />
-                                </svg>
-                                Create
-                            </label>
-                            <ul
-                                tabIndex={0}
-                                className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52"
-                            >
-                                <li>
-                                    <Link href="/new/palette"
-                                          className="flex font-semibold items-center active:bg-black">
-                                        <svg
-                                            xmlns="http://www.w3.org/2000/svg"
-                                            viewBox="0 0 24 24"
-                                            fill="currentColor"
-                                            className="w-6 h-6 mr-2"
-                                        >
-                                            <path
-                                                fillRule="evenodd"
-                                                d="M2.25 4.125c0-1.036.84-1.875 1.875-1.875h5.25c1.036 0 1.875.84 1.875 1.875V17.25a4.5 4.5 0 11-9 0V4.125zm4.5 14.25a1.125 1.125 0 100-2.25 1.125 1.125 0 000 2.25z"
-                                                clipRule="evenodd"
-                                            />
-                                            <path
-                                                d="M10.719 21.75h9.156c1.036 0 1.875-.84 1.875-1.875v-5.25c0-1.036-.84-1.875-1.875-1.875h-.14l-8.742 8.743c-.09.089-.18.175-.274.257zM12.738 17.625l6.474-6.474a1.875 1.875 0 000-2.651L15.5 4.787a1.875 1.875 0 00-2.651 0l-.1.099V17.25c0 .126-.003.251-.01.375z"/>
-                                        </svg>
-                                        Palette
-                                    </Link>
-                                </li>
-                                <li>
-                                    <Link href="/new/gradient"
-                                          className="flex font-semibold items-center active:bg-black">
-                                        <svg
-                                            xmlns="http://www.w3.org/2000/svg"
-                                            viewBox="0 0 24 24"
-                                            fill="currentColor"
-                                            className="w-6 h-6 mr-2"
-                                        >
-                                            <path
-                                                d="M5.566 4.657A4.505 4.505 0 016.75 4.5h10.5c.41 0 .806.055 1.183.157A3 3 0 0015.75 3h-7.5a3 3 0 00-2.684 1.657zM2.25 12a3 3 0 013-3h13.5a3 3 0 013 3v6a3 3 0 01-3 3H5.25a3 3 0 01-3-3v-6zM5.25 7.5c-.41 0-.806.055-1.184.157A3 3 0 016.75 6h10.5a3 3 0 012.683 1.657A4.505 4.505 0 0018.75 7.5H5.25z"/>
-                                        </svg>
-                                        Gradient
-                                    </Link>
-                                </li>
-                                <li>
-                                    <Link href="/new/radial-gradient"
-                                          className="flex font-semibold items-center active:bg-black">
-                                        <svg
-                                            xmlns="http://www.w3.org/2000/svg"
-                                            viewBox="0 0 24 24"
-                                            fill="currentColor"
-                                            className="w-6 h-6 mr-2"
-                                        >
-                                            <path
-                                                d="M5.566 4.657A4.505 4.505 0 016.75 4.5h10.5c.41 0 .806.055 1.183.157A3 3 0 0015.75 3h-7.5a3 3 0 00-2.684 1.657zM2.25 12a3 3 0 013-3h13.5a3 3 0 013 3v6a3 3 0 01-3 3H5.25a3 3 0 01-3-3v-6zM5.25 7.5c-.41 0-.806.055-1.184.157A3 3 0 016.75 6h10.5a3 3 0 012.683 1.657A4.505 4.505 0 0018.75 7.5H5.25z"/>
-                                        </svg>
-                                        Radial Gradient
-                                    </Link>
-                                </li>
-                            </ul>
-                        </div>
-                    </li>
-                </ol>
-            </div>
+            <div className="flex">
+                    {
+                        menuItems.map((item, index) => (
+                            <NavLink key={index} path={item.path} label={item.label} icon={item.icon} />
+                        ))
+                    }
+                </div>
         </nav>
     );
 }
