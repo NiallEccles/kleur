9f62084e1fcc63d73a3698bb7d5a868c2e95dd2b
EcclesN <Niall.Eccles@hmhco.com>
2025-04-12 18:17:57 +0100

    Optional colour

--- components/colour-picker/colour-picker.tsx
+++ components/colour-picker/colour-picker.tsx
@@ -5,7 +5,7 @@ type ColourPickerProps = {
     controls: string[];
     updateSingleControl: (index: number, color: string) => void;
     currentControlIndex: number;
-    color: string; // Optional: this prop isn't used in the component
+    color?: string; // Optional: this prop isn't used in the component
 };
 
 function ColourPicker({ controls, updateSingleControl, currentControlIndex, color }: ColourPickerProps) {
