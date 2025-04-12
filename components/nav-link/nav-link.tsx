eaf1fdf5f2a0f1aa55049326952ac2c98fc7152b
EcclesN <Niall.Eccles@hmhco.com>
2025-04-12 18:11:19 +0100

    Add in types

--- components/nav-link/nav-link.tsx
+++ components/nav-link/nav-link.tsx
@@ -1,14 +1,22 @@
 import Link from "next/link";
+import { ReactNode } from "react";
 
-const NavLink = ({path, icon, label}) => {
+type NavLinkProps = {
+    path: string;
+    icon: ReactNode;
+    label: string;
+};
+
+const NavLink = ({ path, icon, label }: NavLinkProps) => {
     return (
-        <Link href={path} className="flex text-lg md:text-lg font-semibold items-center normal-case ml-5 mb-2 lg:mb-0">
+        <Link
+            href={path}
+            className="flex text-lg md:text-lg font-semibold items-center normal-case ml-5 mb-2 lg:mb-0"
+        >
             {icon}
-            <span className='ml-2'>
-                {label}
-            </span>
+            <span className="ml-2">{label}</span>
         </Link>
-    )
+    );
 };
 
 export default NavLink;
