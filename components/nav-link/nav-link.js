1e941f8d4c78ea25dfed43bb26a8542b4aee0a09
EcclesN <Niall.Eccles@hmhco.com>
2025-04-12 16:20:38 +0100

    Remove ghost button

--- components/nav-link/nav-link.js
+++ components/nav-link/nav-link.js
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 const NavLink = ({path, icon, label}) => {
     return (
-        <Link href={path} className="flex text-lg md:text-lg font-semibold items-center btn btn-ghost normal-case">
+        <Link href={path} className="flex text-lg md:text-lg font-semibold items-center normal-case ml-5 mb-2 lg:mb-0">
             {icon}
             <span className='ml-2'>
                 {label}
