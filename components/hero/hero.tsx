9828758bcdb7307d22293251cfbde63266f8e851
EcclesN <Niall.Eccles@hmhco.com>
2025-04-17 19:54:46 +0100

    Adjust text sizes

--- components/hero/hero.tsx
+++ components/hero/hero.tsx
@@ -10,11 +10,8 @@ export default function Hero() {
       <div className="my-8 py-20 sm:py-40 rounded-3xl">
         <h1 className="text-5xl sm:text-9xl font-bold text-center">kleur</h1>
         <h2 className="text-xl font-bold text-center">
-          <span className="italic">colour</span> (English)
+            All about colour.
         </h2>
-        <h3 className="text-2xl sm:text-4xl font-bold text-center mt-16">
-          All about colour.
-        </h3>
       </div>
   );
 }
