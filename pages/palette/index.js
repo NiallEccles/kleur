c15c2d2c295ce73aaa2217f7843da1d443c1a918
EcclesN <Niall.Eccles@hmhco.com>
2025-09-06 13:59:12 +0100

    Palette display

--- pages/palette/index.js
+++ pages/palette/index.js
@@ -2,25 +2,34 @@ import styles from "../../styles/Home.module.css";
 import Nav from "../../components/nav/nav";
 import { useEffect, useState } from "react";
 import Palette from "../../components/palette/palette";
+import {useSearchParams} from "next/navigation";
+import {getFromStorage} from "../../hooks/getFromStorage.hook";
+import PaletteGenerator from "../../components/palette-generator/palette-generator";
 
 export default function Index() {
   const [palette, setPalette] = useState([]);
   const [name, setName] = useState();
+  const searchParams = useSearchParams();
+
+  const data = getFromStorage('palettes', searchParams.get("name"));
+
+  console.log(data)
 
   useEffect(() => {
     if (typeof window !== "undefined") {
       let url = new URL(window.location.href);
       let params = new URLSearchParams(url.search);
+      console.log(params)
 
-      const paletteColours = params.get("p");
-      const formattedColours = paletteColours
-        .split(",")
-        .map((colour) => `#${colour}`);
-
-      setPalette(formattedColours);
-
-      const paletteName = params.get("name");
-      setName(paletteName);
+      // const paletteColours = params.get("p");
+      // const formattedColours = paletteColours
+      //   .split(",")
+      //   .map((colour) => `#${colour}`);
+      //
+      // setPalette(formattedColours);
+      //
+      // const paletteName = params.get("name");
+      // setName(paletteName);
     }
   }, []);
 
@@ -29,10 +38,11 @@ export default function Index() {
       <Nav />
       <div className="flex justify-center">
         <div>
-          <Palette colours={palette} />
-          <div className="px-8 font-semibold">
-            <span className="tag">{name}</span>
-          </div>
+          <PaletteGenerator name={data?.paletteName} cols={data?.colors} />
+          {/*<Palette colours={palette} />*/}
+          {/*<div className="px-8 font-semibold">*/}
+          {/*  <span className="tag">{name}</span>*/}
+          {/*</div>*/}
         </div>
       </div>
     </div>
