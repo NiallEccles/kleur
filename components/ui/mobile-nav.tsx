8fe586abe26fe9208b0c2f421b0d8e80a171db03
Eccles, Niall <n.eccles14@gmail.com>
2026-02-24 22:51:57 +0000

    Add bold font for titles

--- components/ui/mobile-nav.tsx
+++ components/ui/mobile-nav.tsx
@@ -7,7 +7,7 @@ import { useRouter } from "next/navigation"
 import { cn } from "@/lib/utils";
 import {Button} from "@/components/ui/button";
 import {Drawer, DrawerContent, DrawerTrigger} from "@/components/ui/drawer";
-import {MenuItem, NewMenuItems} from "@/types/MenuItem";
+import {NewMenuItems} from "@/types/MenuItem";
 import {Menu} from "lucide-react";
 
 export function MobileNav({ menuItems }: { menuItems: NewMenuItems }){
@@ -16,7 +16,6 @@ export function MobileNav({ menuItems }: { menuItems: NewMenuItems }){
     const onOpenChange = React.useCallback(
         (open: boolean) => {
             setOpen(open)
-            // setMetaColor(open ? "#09090b" : metaColor)
         },
         []
     )
@@ -28,20 +27,6 @@ export function MobileNav({ menuItems }: { menuItems: NewMenuItems }){
                     variant="ghost"
                     className="gap-4 px-0 text-base hover:bg-transparent focus-visible:bg-transparent focus-visible:ring-0 focus-visible:ring-offset-0 sm:hidden"
                 >
-                    {/*<svg*/}
-                    {/*    xmlns="http://www.w3.org/2000/svg"*/}
-                    {/*    fill="none"*/}
-                    {/*    viewBox="0 0 24 24"*/}
-                    {/*    strokeWidth="1.5"*/}
-                    {/*    stroke="currentColor"*/}
-                    {/*    className="!size-6"*/}
-                    {/*>*/}
-                    {/*    <path*/}
-                    {/*        strokeLinecap="round"*/}
-                    {/*        strokeLinejoin="round"*/}
-                    {/*        d="M3.75 9h16.5m-16.5 6.75h16.5"*/}
-                    {/*    />*/}
-                    {/*</svg>*/}
                     <Menu className='!size-6' />
                     <span className="sr-only">Toggle Menu</span>
                 </Button>
@@ -52,8 +37,8 @@ export function MobileNav({ menuItems }: { menuItems: NewMenuItems }){
                         {
                             menuItems.map((menuItem) => (
                                 <>
-                                    <h2 className='text-xl mb-0' key={menuItem.label}>{menuItem.label}</h2>
-                                    <ol className='ml-3'>
+                                    <h2 className='text-xl font-bold mb-0' key={menuItem.label}>{menuItem.label}</h2>
+                                    <ol className='ml-6'>
                                         {
                                             menuItem.subItems.map((subItem) => (
                                                 <li className='my-2' key={subItem.href}>
@@ -70,35 +55,6 @@ export function MobileNav({ menuItems }: { menuItems: NewMenuItems }){
                                 </>
                             ))
                         }
-                    </div>
-                    <div className="flex flex-col space-y-2">
-                        {/*{docsConfig.sidebarNav.map((item, index) => (*/}
-                        {/*    <div key={index} className="flex flex-col gap-4 pt-6">*/}
-                        {/*        <h4 className="text-xl font-medium">{item.title}</h4>*/}
-                        {/*        {item?.items?.length &&*/}
-                        {/*            item.items.map((item) => (*/}
-                        {/*                <React.Fragment key={item.href}>*/}
-                        {/*                    {!item.disabled &&*/}
-                        {/*                        (item.href ? (*/}
-                        {/*                            <MobileLink*/}
-                        {/*                                href={item.href}*/}
-                        {/*                                onOpenChange={setOpen}*/}
-                        {/*                                className="opacity-80"*/}
-                        {/*                            >*/}
-                        {/*                                {item.title}*/}
-                        {/*                                {item.label && (*/}
-                        {/*                                    <span className="ml-2 rounded-md bg-[#adfa1d] px-1.5 py-0.5 text-xs leading-none text-[#000000] no-underline group-hover:no-underline">*/}
-                        {/*        {item.label}*/}
-                        {/*      </span>*/}
-                        {/*                                )}*/}
-                        {/*                            </MobileLink>*/}
-                        {/*                        ) : (*/}
-                        {/*                            item.title*/}
-                        {/*                        ))}*/}
-                        {/*                </React.Fragment>*/}
-                        {/*            ))}*/}
-                        {/*    </div>*/}
-                        {/*))}*/}
                     </div>
                 </div>
             </DrawerContent>
