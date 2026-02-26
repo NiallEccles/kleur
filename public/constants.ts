13a4e6d388490c01141d4dcf75df5fa282699803
Eccles, Niall <niall.eccles@hmhco.com>
2026-02-26 22:08:12 +0000

    Add HOME,PALETTES,GRADIENTS,BRAND_KIT,CONVERT to constants

--- public/constants.ts
+++ public/constants.ts
@@ -1,5 +1,9 @@
 export const PAGES = {
   HOME: "/",
+  PALETTES: "/palettes",
+  GRADIENTS: "/gradients",
+  BRAND_KIT: "/brand-kit",
+  CONVERT: "/convert",
   NEW: {
     PALETTE: "/create/palette",
     GRADIENT: "/create/gradient"
