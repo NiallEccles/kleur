e50b0c48d1c44a8b89b97ce3d2669477d5fdaed1
Eccles, Niall <niall.eccles@hmhco.com>
2026-02-27 19:03:35 +0000

    Add 100dvh height - 100px

--- components/generator/GeneratorLayout.tsx
+++ components/generator/GeneratorLayout.tsx
@@ -9,8 +9,8 @@ interface GeneratorLayoutProps {
 
 export default function GeneratorLayout({ canvas, panel }: GeneratorLayoutProps) {
   return (
-    <div className="grid gap-8 md:grid-cols-[1fr_300px]">
-      <div className="relative">{canvas}</div>
+    <div className="grid gap-8 md:grid-cols-[1fr_300px] h-[calc(100dvh-100px)]">
+      <div className="relative h-full">{canvas}</div>
       <div className="space-y-4">{panel}</div>
     </div>
   )
