c1aa04897f7a0f7168864cb1720c11033f13a464
Eccles, Niall <niall.eccles@hmhco.com>
2026-02-28 10:05:54 +0000

    Min height 10dvh for mobile

--- components/generator/GeneratorLayout.tsx
+++ components/generator/GeneratorLayout.tsx
@@ -10,7 +10,7 @@ interface GeneratorLayoutProps {
 export default function GeneratorLayout({ canvas, panel }: GeneratorLayoutProps) {
   return (
     <div className="grid gap-8 md:grid-cols-[1fr_300px] h-[calc(100dvh-100px)]">
-      <div className="relative h-full">{canvas}</div>
+      <div className="relative h-full min-h-[10dvh]">{canvas}</div>
       <div className="space-y-4">{panel}</div>
     </div>
   )
