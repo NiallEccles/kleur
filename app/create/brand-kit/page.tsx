d5b7e143755468c0b4b50569283440fb095edfbe
Eccles, Niall <niall.eccles@hmhco.com>
2026-03-04 23:01:42 +0000

    Add ability to create brand kit

---
+++ app/create/brand-kit/page.tsx
@@ -0,0 +1,11 @@
+"use client"
+
+import BrandKitV3 from "@/components/brand-kit-v3/BrandKitV3"
+
+export default function CreateGradientPage() {
+  return (
+    <div className="mx-5 space-y-12">
+      <BrandKitV3 />
+    </div>
+  )
+}
